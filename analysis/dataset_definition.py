c37dbea89483a27a5923413cc49572c522b61fc3
milanwiedemann <milan.wiedemann@gmail.com>
2022-10-10 12:15:46 +0100

    Add initial dataset definition and dummy data
    This commit adds some example patients and some dataset restrictions.

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,7 +1,21 @@
 from databuilder.ehrql import Dataset, case, codelist_from_csv, when
 from databuilder.tables.beta.tpp import patients, practice_registrations
+from datetime import date
 
 dataset = Dataset()
 
+# Extract variables
+dataset.sex = patients.sex
+dataset.age = patients.date_of_birth.difference_in_years(date.today())
 
-dataset.set_population(practice_registrations.exists_for_patient())
+# Define dataset restrictions
+set_registered = practice_registrations.exists_for_patient()
+set_sex_not_null = dataset.sex.is_not_null()
+set_sex_fm = (dataset.sex == "F") | (dataset.sex == "M")
+set_age_ge2_le120 = (dataset.age >= 2) & (dataset.age <= 120)
+
+# Set 
+dataset.set_population(set_registered 
+                       & set_sex_not_null & set_sex_fm
+                       & set_age_ge2_le120)
+
