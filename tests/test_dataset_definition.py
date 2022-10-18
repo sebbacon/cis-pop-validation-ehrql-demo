97683d4eabc0353b711022f1b347a021eef1de33
milanwiedemann <milan.wiedemann@gmail.com>
2022-10-18 18:57:43 +0100

    Add dataset tests for new variables

--- tests/test_dataset_definition.py
+++ tests/test_dataset_definition.py
@@ -1,6 +1,7 @@
 # To test dataset definition run the following command in the terminal:
 # pytest tests/test_dataset_definition.py
 import os
+
 import pandas as pd
 import pytest
 
@@ -14,8 +15,32 @@ def test_dataset_definition():
     pt2 = df_output.iloc[0].to_dict()
     pt3 = df_output.iloc[1].to_dict()
     pt7 = df_output.iloc[2].to_dict()
-    
+
     # assert
-    assert pt2 == {'patient_id': 2, 'sex': 'F', 'age': 72, 'has_died': 'F'}
-    assert pt3 == {'patient_id': 3, 'sex': 'F', 'age': 62, 'has_died': 'F'}
-    assert pt7 == {'patient_id': 7, 'sex': 'M', 'age': 22, 'has_died': 'F'}
+    assert pt2 == {"patient_id": 2,
+                   "sex": "F",
+                   "age": 72,
+                   "has_died": "F",
+                   "care_home_tpp": "F",
+                   "care_home_code": "T",
+                   "msoa": 1,
+                   "stp": 1,
+                   "region": "region_practice1"}
+    assert pt3 == {"patient_id": 3,
+                   "sex": "F",
+                   "age": 62,
+                   "has_died": "F",
+                   "care_home_tpp": "T",
+                   "care_home_code": "F",
+                   "msoa": 1,
+                   "stp": 2,
+                   "region": "region_practice2"}
+    assert pt7 == {"patient_id": 7,
+                   "sex": "M",
+                   "age": 22,
+                   "has_died": "F",
+                   "care_home_tpp": "F",
+                   "care_home_code": "F",
+                   "msoa": 1,
+                   "stp": 3,
+                   "region": "region_practice3"}
